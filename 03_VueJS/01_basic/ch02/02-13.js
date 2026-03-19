e4367424e9f35bcd03990c1442871c6f2f655900
shpark47 <shp04078@naver.com>
2026-03-19 16:31:09 +0900

    vue 기초, 템플릿 오후수업

--- 03_VueJS/01_basic/ch02/02-13.js
+++ 03_VueJS/01_basic/ch02/02-13.js
@@ -4,6 +4,9 @@ obj.add = function (x, y) {
     this.result = x + y;
   }
   inner();
+  // JavaScript에서 일반 함수의 this는 호출 방식에 따라 결정됨
+  // inner() 함수는 단독으로 호출되었기 때문에, 그 안의 this는
+  // 전역 객체(브라우저에서는 window, Node.js에서는 global)를 가리킴
 };
 
 obj.add(3, 4);
