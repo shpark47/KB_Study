ab1f5ae3b3ef1d6ea5b0bd21dffd9515ce82b75d
shpark47 <shp04078@naver.com>
2026-04-01 08:39:52 +0900

    UIUX특강 2일차

--- 00_특강/UIUX/code/1일차/2. 좋은 UX를 판단하는 기준/script.js
+++ 00_특강/UIUX/code/1일차/2. 좋은 UX를 판단하는 기준/script.js
@@ -1,33 +1,20 @@
-const loginBtn = document.getElementById('loginBtn');
-const errorBox = document.getElementById('errorBox');
-const successBox = document.getElementById('successBox');
-const id = document.getElementById('userId');
-const pw = document.getElementById('userPw');
+const loginBtn = document.getElementById("loginBtn");
+const errorBox = document.getElementById("errorBox");
 
 // 메시지 초기화 함수
 // 로그인 시도 전에 기존 메시지를 모두 숨기고 초기화
 function resetMessages() {
   // 메시지 영역 숨기기
-  errorBox.classList.add('hidden');
-  successBox.classList.add('hidden');
-  helperLink.classList.add('hidden');
+  errorBox.classList.add("hidden");
+  successBox.classList.add("hidden");
+  helperLink.classList.add("hidden");
 
   // 메시지 내용 초기화
-  errorBox.textContent = '';
-  successBox.textContent = '';
+  errorBox.textContent = "";
+  successBox.textContent = "";
 }
 
-loginBtn.addEventListener('click', () => {
-  resetMessages();
-  if (id.value == 'admin' && pw.value == '1234') {
-    successBox.textContent = '로그인에 성공했습니다.';
-    successBox.classList.remove('hidden');
-  } else {
-    if (id.value != 'admin') {
-      errorBox.textContent = '아이디가 틀렸습니다.';
-    } else {
-      errorBox.textContent = '비밀번호가 틀렸습니다.';
-    }
-    errorBox.classList.remove('hidden');
-  }
+loginBtn.addEventListener("click", () => {
+  errorBox.textContent = "오류가 발생했습니다.";
+  errorBox.classList.remove("hidden");
 });
