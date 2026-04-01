7dfea2801b05703ccc38b39acd9292c6bacaff13
shpark47 <shp04078@naver.com>
2026-04-01 17:05:07 +0900

    1주차 과제 완료

--- 과제/router-test/src/pages/About.vue
+++ 과제/router-test/src/pages/About.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="card card-body">
     <h2>About</h2>
-    <p>요청 경로 : {{ $route.fullPath }}</p>
+    <!-- <p>요청 경로 : {{ $route.fullPath }}</p> -->
+    <p>요청 경로 : {{ route.fullPath }}</p>
   </div>
 </template>
 
-<script>
+<!-- <script>
 export default {
   name: 'About',
   created() {
     console.log(this.$route);
   },
 };
+</script> -->
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+console.log(route);
 </script>
+
 <style scoped></style>
