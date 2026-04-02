c1be50b010b63b7596dd02f6730c13da4124d4e9
shpark47 <shp04078@naver.com>
2026-04-02 09:05:10 +0900

    04.01 기본실습

--- 03_VueJS/01_basic/10_Router/router-test/src/pages/MemberInfo.vue
+++ 03_VueJS/01_basic/10_Router/router-test/src/pages/MemberInfo.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="card card-body">
     <h2>Member Info</h2>
-    <div>
-      경로 패턴 : /members/:id<br />
-      요청 경로 : {{ route.fullPath }}<br />
-      id값 : {{ route.params.id }}<br />
+    <div className="mt-5">
+      <img :src="member.photo" class="img" />
+      <h4 class="mt 2">{{ member.name }}({{ member.role }})</h4>
+      <p>{{ member.desc }}</p>
+      <a
+        v-if="member.insta && member.insta !== ''"
+        class="fa fa-instagram m-1"
+        :href="member.insta"
+      ></a>
+      <a
+        v-if="member.facebook && member.facebook !== ''"
+        class="fa fa-facebook m-1"
+        :href="member.facebook"
+      ></a>
+      <a
+        v-if="member.youtube && member.youtube !== ''"
+        class="fa fa-youtube m-1"
+        :href="member.youtube"
+      ></a>
+      <br />
+      <br />
+      <router-link to="/members"> 멤버 목록으로 </router-link>
     </div>
   </div>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router';
+import members from '@/members.json';
 
 const route = useRoute();
+const id = parseInt(route.params.id, 10);
+const member = members.find((m) => m.id === id);
 </script>
 
 <style scoped></style>
--- 03_VueJS/01_basic/10_Router/router-test/src/pages/Members.vue
+++ 03_VueJS/01_basic/10_Router/router-test/src/pages/Members.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="card card-body">
-    <h2>Members</h2>
+    <h2 class="m-4">이날치 멤버</h2>
+    <div class="container">
+      <div class="row">
+        <div
+          v-for="m in members"
+          :key="m.id"
+          class="col-6 col-xs-6 col-sm-4 col-md-3 col-lg-2"
+        >
+          <router-link :to="'/members/' + m.id">
+            <img
+              class="img-thumbnail"
+              style="width: 90px; height: 110px"
+              :src="m.photo"
+              :title="m.name"
+            /><br />
+            <h6 class="display-7">{{ m.name }}</h6>
+          </router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +28,8 @@ export default {
   name: 'Members',
 };
 </script>
-<script setup></script>
+<script setup>
+import members from '@/members.json';
+</script>
 
 <style scoped></style>
