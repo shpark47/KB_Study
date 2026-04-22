b1914260507479aed5bf39d8cdc8e67929681ac3
seonghun <shp04078@naver.com>
2026-04-22 15:01:15 +0900

    Study. 04.22 3번째 과제 커밋

---
+++ 과제/java_week_3/src/ch08/sec04/Television.java
@@ -0,0 +1,23 @@
+package ch08.sec04;
+
+public class Television implements RemoteControl{
+    private int volume;
+
+    @Override
+    public void turnOn() {
+        System.out.println("TV를 켭니다.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("TV를 끕니다.");
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        if(volume < MIN_VOLUME) volume = MIN_VOLUME;
+        if(volume > MAX_VOLUME) volume = MAX_VOLUME;
+        this.volume = volume;
+        System.out.println("현재 TV 볼륨: " + this.volume);
+    }
+}
