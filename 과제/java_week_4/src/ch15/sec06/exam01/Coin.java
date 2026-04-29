a9b11ad415437440ffc136f4a69c6acbaee43fbc
seonghun <shp04078@naver.com>
2026-04-29 14:24:03 +0900

    Fix. 04.29 4주차 과제 수정

--- 과제/java_week_4/src/ch15/sec06/exam01/Coin.java
+++ 과제/java_week_4/src/ch15/sec06/exam01/Coin.java
@@ -1,13 +1,10 @@
 package ch15.sec06.exam01;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Coin {
     private int value;
-
-    public Coin(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
 }
