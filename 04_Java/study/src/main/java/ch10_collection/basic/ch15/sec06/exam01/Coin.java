75dc4c907279d8e52533aba9194b6f3a2702655b
seonghun <shp04078@naver.com>
2026-04-29 15:53:29 +0900

    Study. 04.29 기본실습 커밋

---
+++ 04_Java/study/src/main/java/ch10_collection/basic/ch15/sec06/exam01/Coin.java
@@ -0,0 +1,10 @@
+package main.java.ch10_collection.basic.ch15.sec06.exam01;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Coin {
+    private int value;
+}
---
+++ 04_Java/study/src/main/java/ch10_collection/basic/ch15/sec06/exam02/Message.java
@@ -0,0 +1,9 @@
+package main.java.ch10_collection.basic.ch15.sec06.exam02;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Message {
+    public String command;
+    public String to;
+}
